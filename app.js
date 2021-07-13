7ae30adcfa4ca90db6c4fc0fef660bf75a25314b
MudrikaAlla <77771797+MudrikaAlla@users.noreply.github.com>
2021-07-13 17:27:57 +0530

    finished input output processing

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    outputDiv.innerText = "akhbdvkasjb " + txtInput.value;
-})
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Error occured: " + error);
+    console.log("The server might be down, please try again later");
+}
+
+function clickEventHandler() {
+
+    var inputText = txtInput.value; //taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            // console.log(json.contents.translated)
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickEventHandler)
